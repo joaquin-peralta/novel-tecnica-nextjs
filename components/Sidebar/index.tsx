10776d89474c73aa12b3a08f1da50010c99f4e00
Joaquin Peralta <joaquinperalta1406@gmail.com>
2021-06-11 11:57:15 -0300

    Added: sidebar animation

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -4,10 +4,25 @@ import styles from '@styles/components/Sidebar.module.scss';
 import NavData from '@components/Navbar/NavData.json';
 import Link from 'next/link';
 import { AiOutlineClose } from 'react-icons/ai';
+import { gsap } from 'gsap';
 
 const Sidebar = () => {
   const { show, toggle } = useContext(SidebarContext);
   const sidebarRef = useRef(null);
+  const backLayerRef = useRef(null);
+  const t1 = gsap.timeline({ paused: true });
+
+  useEffect(() => {
+    if (show) {
+      t1.to(sidebarRef.current, { x: 0 })
+        .to(backLayerRef.current, { xPercent: -100, opacity: 1, autoAlpha: 1 }, '-=0.5')
+        .play();
+    } else {
+      t1.to(sidebarRef.current, { x: 280 })
+        .to(backLayerRef.current, { xPercent: 100, opacity: 0, autoAlpha: 0 }, '-=0.3')
+        .play();
+    }
+  }, [show]);
 
   const useOutsideAlerter = (ref: any) => {
     useEffect(() => {
@@ -27,26 +42,29 @@ const Sidebar = () => {
   useOutsideAlerter(sidebarRef);
 
   return (
-    <aside className={show ? styles.sidebarOpen : styles.sidebar}>
-      <div className={styles.buttonContainer}>
-        <button className={styles.closeButton} onClick={toggle} type="button">
-          <AiOutlineClose style={{ fontSize: '24px', color: '#525353' }} />
-        </button>
-      </div>
-      <nav ref={sidebarRef} className="text-right">
-        <ul className={styles.navList}>
-          {NavData.map((item) => (
-            <Link key={item.title} href={item.path} passHref>
-              <li className={styles.item}>
-                <a className={styles.navAnchor} onClick={toggle}>
-                  {item.title}
-                </a>
-              </li>
-            </Link>
-          ))}
-        </ul>
-      </nav>
-    </aside>
+    <>
+      <aside ref={sidebarRef} className={styles.sidebar}>
+        <div className={styles.buttonContainer}>
+          <button className={styles.closeButton} onClick={toggle} type="button">
+            <AiOutlineClose style={{ fontSize: '24px', color: '#525353' }} />
+          </button>
+        </div>
+        <nav className="text-right">
+          <ul className={styles.navList}>
+            {NavData.map((item) => (
+              <Link key={item.title} href={item.path} passHref>
+                <li className={styles.item}>
+                  <a className={styles.navAnchor} onClick={toggle}>
+                    {item.title}
+                  </a>
+                </li>
+              </Link>
+            ))}
+          </ul>
+        </nav>
+      </aside>
+      <div ref={backLayerRef} className={styles.backLayer} />
+    </>
   );
 };
 
